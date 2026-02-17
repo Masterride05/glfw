ea701febbc5457ecd7763397c76b1f58077746fb
Masterride <43613854+Masterride05@users.noreply.github.com>
2026-02-17 22:27:30 +0100

    Refactor GLFW project file structure and settings

--- premake5.lua
+++ premake5.lua
@@ -1,54 +1,35 @@
 project "GLFW"
     kind "StaticLib"
     language "C"
-    staticruntime "on"
 
     targetdir ("bin/" .. outputdir .. "/%{prj.name}")
     objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
-    files
-    {
-        "include/GLFW/glfw3.h",
-        "include/GLFW/glfw3native.h",
-        "src/glfw_config.h",
-        "src/context.c",
-        "src/init.c",
-        "src/input.c",
-        "src/monitor.c",
-        "src/vulkan.c",
-        "src/window.c"
+    files {
+        "src/**.c",
+        "include/**.h"
+    }
+
+    includedirs {
+        "include"
     }
 
     filter "system:windows"
+        staticruntime "on"
         systemversion "latest"
-
-        files
-        {
-            "src/win32_init.c",
-            "src/win32_joystick.c",
-            "src/win32_monitor.c",
-            "src/win32_time.c",
-            "src/win32_thread.c",
-            "src/win32_window.c",
-            "src/wgl_context.c",
-            "src/egl_context.c",
-            "src/osmesa_context.c"
+        defines { "_GLFW_WIN32" }
+        files {
+            "src/win32_*.c",
+            "src/wgl_context.c"
         }
 
-        defines 
-        { 
-            "_GLFW_WIN32",
-            "_CRT_SECURE_NO_WARNINGS"
+    filter "system:linux"
+        pic "On"
+        defines { "_GLFW_X11" }
+        files {
+            "src/x11_*.c",
+            "src/glx_context.c"
         }
+        links { "X11", "GL", "pthread", "dl" }
 
-    filter "configurations:Debug"
-        runtime "Debug"
-        symbols "on"
-
-    filter "configurations:Release"
-        runtime "Release"
-        optimize "on"
-
-    filter "configurations:Dist"
-        runtime "Release"
-        optimize "on"
+    filter {}
