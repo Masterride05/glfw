1c10290630fb864035bac1d8ec331006d348f703
Masterride <masterrobi05@gmail.com>
2026-02-18 14:24:04 +0100

    premake updated

--- premake5.lua
+++ premake5.lua
@@ -1,9 +1,10 @@
 project "GLFW"
-    kind "StaticLib"
+    kind "SharedLib"
     language "C"
+    staticruntime "off"
 
-    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+    targetdir ("bin/" .. outputdir)
+    objdir ("bin-int/" .. outputdir)
 
     files {
         "src/**.c",
@@ -15,21 +16,47 @@ project "GLFW"
     }
 
     filter "system:windows"
-        staticruntime "on"
         systemversion "latest"
-        defines { "_GLFW_WIN32" }
+
+        defines {
+            "_GLFW_WIN32",
+            "_CRT_SECURE_NO_WARNINGS",
+            "_GLFW_BUILD_DLL"
+        }
+
         files {
             "src/win32_*.c",
             "src/wgl_context.c"
         }
+        links {
+            "gdi32",
+            "user32",
+            "shell32",
+            "opengl32"
+        }
+        postbuildcommands {
+            {"{COPY} %{cfg.buildtarget.relpath} ../../bin/" .. outputdir}
+            -- {"{COPY} %{cfg.buildtarget.relpath} ../../"},
+        }
 
     filter "system:linux"
         pic "On"
-        defines { "_GLFW_X11" }
+        defines { 
+            "_GLFW_X11",
+            "_GLFW_BUILD_DLL" 
+        }
         files {
             "src/x11_*.c",
             "src/glx_context.c"
         }
-        links { "X11", "GL", "pthread", "dl" }
+
+        links {
+            "X11",
+            "Xrandr",
+            "Xi",
+            "dl",
+            "pthread",
+            "GL"
+        }
 
     filter {}
